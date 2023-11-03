72da5ba510255a85dbad3e5601ed214554528525
Israel <smr.iazofra.1971@iespabloserrano.com>
2023-11-03 13:53:14 +0100

    IAW

---
+++ Proyecto/usuarios.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>inicio de Sesion</title>
+</head>
+<body>
+    <form action="usuariosguardados.php" method="get">
+        <label form>Nombre de Usuario</label>
+        <input type="text" name="Nombre">
+        <br>
+        <label form>Contraseña</label>
+        <input type="text" name="Contraseña">
+        <br>
+        <br>
+        <input type="submit">
+    </form>
+</body>
+</html>
