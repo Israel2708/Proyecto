d92f7b4d56133fbc6030f790bf91e1389adfe09c
Israel <smr.iazofra.1971@iespabloserrano.com>
2023-11-09 14:17:12 +0100

    Update añadircarrito.php

--- Proyecto/añadircarrito.php
+++ Proyecto/añadircarrito.php
@@ -12,7 +12,12 @@
     session_start();
     $_SESSION['msg'] = "Añadido al carrito";
     //Declaramos el array
-    $idguardados = array();
+    $idguardadoslocal = array();
+    //Si ya existe el array con el idguardadoslocal en sesion lo recupero
+    //para no sobreescribirlo
+    if (isset($_SESSION['idguardados'])) {
+        
+    }
     //Redirigo de vuelta a la pagina de productos
     Header("Location: index.php");
 ?>
