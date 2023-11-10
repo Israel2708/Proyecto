bef2aea0864a2083339ad9dece6e9463556f40f0
Israel <smr.iazofra.1971@iespabloserrano.com>
2023-11-10 13:14:09 +0100

    Update detalle_pedido.php

--- Proyecto/detalle_pedido.php
+++ Proyecto/detalle_pedido.php
@@ -1,7 +1,13 @@
 <?php
-//Recojo en una variable el array de ID guarddo en session_id
+//Recojo en una variable el array de ID guarddo en session
+$detalle_pedido = $_SESSION['productospedidos']
 //Bucle de ese array
+for ($i=0; $i=$_SESSION['productospedidos'] ; $i++) { 
+    # code...
+}
 //Por cada ID contruyo un select where id=idproducto
+$sql= //"select * from productos where id = id"
 //Recojo la informacion
+
 //La muestro en una linea
 ?>
