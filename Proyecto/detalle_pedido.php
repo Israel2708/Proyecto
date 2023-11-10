c4fe4d03ce12a0485c6fe600c81f143d3ba8bdf9
Israel <smr.iazofra.1971@iespabloserrano.com>
2023-11-10 13:05:32 +0100

    Create detalle_pedido.php

---
+++ Proyecto/detalle_pedido.php
@@ -0,0 +1,7 @@
+<?php
+//Recojo en una variable el array de ID guarddo en session_id
+//Bucle de ese array
+//Por cada ID contruyo un select where id=idproducto
+//Recojo la informacion
+//La muestro en una linea
+?>
